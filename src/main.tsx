6423b48cdfe9e2596dff7fe96fe5b74941247f0b
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-06-26 19:45:22 -0400

    hello world has added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM  from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const App = () => {
+  return (
+    <h1>Hello world</h1>
+  )
+}
+
+const element = document.getElementById('root');
+
+if (element) {
+    ReactDOM.createRoot(element).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    )
+}
+
+export default App
