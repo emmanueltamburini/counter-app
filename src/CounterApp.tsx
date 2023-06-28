8eccbc4d0b906cd4de035a423c27ebebd24c1a25
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-06-28 06:12:05 -0400

    onClick event has added

--- src/CounterApp.tsx
+++ src/CounterApp.tsx
@@ -1,12 +1,21 @@
+import { MouseEvent } from "react";
+
 interface Props {
     value: number;
 }
 
 export const CounterApp = ({value}: Props) => {
+  const handleAdd = (event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
+    console.log(event);
+  };
+
   return (
     <>
         <h1>CounterApp</h1>
         <h2> { value } </h2>
+        <button onClick={handleAdd}>
+            +1
+        </button>
     </>
   )
 }
