4d8ab1b9d08c4a2ca2685c4ae278264484c37aa5
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-07-05 06:17:26 -0400

    different kind of tests have added

--- src/HelloWorldApp.tsx
+++ src/HelloWorldApp.tsx
@@ -14,8 +14,9 @@ interface Props {
 const HelloWorldApp = ({title, subTitle, name}: Props) => {
   return (
     <>
-        <h1> {title} </h1>
-        <h2>{subTitle + 1}</h2>
+        <h1 data-testid="test-title"> {title} </h1>
+        <h2 data-testid="test-subTitle">{subTitle + 1}</h2>
+        <h2 data-testid="test-subTitle2">{subTitle + 1}</h2>
         <code>{JSON.stringify(newMessage)}</code>
         <p>{helloWorld(name)}</p>
     </>
--- tests/HelloWorldApp.test.tsx
+++ tests/HelloWorldApp.test.tsx
@@ -10,10 +10,17 @@ describe('HelloWorldApp test', () => {
 
     test('should show an H1 element', () => {
         const title = 'Title';
-        const {container, getByText} = render(<HelloWorldApp name="Emmanuel" title={title} subTitle={2}/>);
+        const subTitle = 2;
+        const {container, getByText, getByTestId, getAllByText} = render(<HelloWorldApp name="Emmanuel" title={title} subTitle={subTitle}/>);
         expect(getByText(title)).toBeTruthy();
+        expect(getByTestId('test-title').innerHTML).toContain(title);
+        expect(getByTestId('test-subTitle').innerHTML).toContain((subTitle + 1).toString());
+        expect(getAllByText((subTitle + 1).toString()).length).toBe(2);
+
 
         const h1 = container.querySelector('h1');
         expect(h1?.innerHTML).toContain(title);
     });
+
+
 })
